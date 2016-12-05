a4fc0e01d0162ad2fce8779600ad276dae64c669
Levi Aul <levi@leviaul.com>
2016-12-05 15:15:56 -0800

    Expose Slack token

--- lib/sms_forwarder/router.ex
+++ lib/sms_forwarder/router.ex
@@ -1,8 +1,16 @@
 defmodule SMSForwarder.Router do
+  require Logger
   use Trot.Router
 
   get "/text" do
     "Thank you for your question."
+  end
+
+  get "/send" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    Logger.info ["Received sendSMS req: ", inspect(conn.params)]
+
+    "ok"
   end
 
   import_routes Trot.NotFound
