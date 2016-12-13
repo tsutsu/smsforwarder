7761dbefef25d9adc6e1abcd9604c1c0d375845d
Levi Aul <levi@leviaul.com>
2016-12-12 16:59:45 -0800

    Split config lines

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,14 @@
 use Mix.Config
 
-config :trot, :port, (System.get_env("PORT") || 8000)
-config :trot, :router, SMSForwarder.VoIPms.WebhookRouter
+web_port = System.get_env("PORT") || 8000
+web_base_uri = System.get_env("BASE_URI") || "http://localhost:#{web_port}"
 
-[twilio_id, twilio_secret] = (System.get_env("TWILIO_CREDENTIALS") || "a:b") |> String.split(":", parts: 2)
+config :trot, :base_uri, URI.parse(web_base_uri)
+config :trot, :port, web_port
+config :trot, :router, SMSForwarder.HTTPRouter
+
+twilio_creds = System.get_env("TWILIO_CREDENTIALS") || "a:b"
+[twilio_id, twilio_secret] = twilio_creds |> String.split(":", parts: 2)
 
 config :ex_twilio,
   account_sid: twilio_id,
