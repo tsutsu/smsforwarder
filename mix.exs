762c2fe95503a75b8055dbde3dc4b57906f4dfc2
Levi Aul <levi@leviaul.com>
2017-09-28 11:12:38 -0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -1,40 +1,28 @@
 defmodule SMSForwarder.Mixfile do
   use Mix.Project
 
-  def project do
-    [app: :sms_forwarder,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
+  def project, do: [
+    app: :sms_forwarder,
+    version: "0.1.0",
+    elixir: "~> 1.5",
+    build_embedded: Mix.env == :prod,
+    start_permanent: Mix.env == :prod,
+    deps: deps()
+  ]
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger, :slack, :ex_twilio, :trot, :calendar, :httpoison, :exredis],
-     mod: {SMSForwarder, []}]
-  end
+  def application, do: [
+    mod: {SMSForwarder, []},
+    extra_applications: [:logger]
+  ]
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do [
-    {:slack, "~> 0.9.0"},
-    {:ex_twilio, "~> 0.2.1"},
-    {:httpoison, "~> 0.9.0"},
+  defp deps, do: [
+    {:slack, "~> 0.12.0"},
+    {:ex_twilio, "~> 0.5.0"},
+    {:httpoison, "~> 0.13.0"},
     {:trot, github: "tsutsu/trot"},
-    {:calendar, "~> 0.16.1"},
-    {:poison, "~> 3.0"},
+    {:calendar, "~> 0.17.4"},
+    {:poison, "~> 3.1"},
     {:exredis, "~> 0.2.5"},
-    {:xml_builder, "~> 0.0.8"}
-  ] end
+    {:xml_builder, "~> 0.1.1"}
+  ]
 end
